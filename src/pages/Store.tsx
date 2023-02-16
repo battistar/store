86ce3ca66872d2ac99b0669f8e85e8c187f68c87
Samuele Battistella <battistar92@gmail.com>
2023-02-16 19:54:35 +0100

    Add loader

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,11 +1,12 @@
 import { Stack, Container, Grid, Pagination, PaginationItem } from '@mui/material';
+import Loader from 'components/Loader';
 import ProductCard from 'components/ProductCard';
 import { ReactNode, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useProduct } from 'store';
 
 const Store = (): JSX.Element => {
-  const { data, fetchData } = useProduct();
+  const { data, fetchData, isLoading } = useProduct();
   const location = useLocation();
 
   const query = new URLSearchParams(location.search);
@@ -16,26 +17,30 @@ const Store = (): JSX.Element => {
   }, [fetchData, page]);
 
   return (
-    <Container maxWidth="lg" sx={{ py: { xs: 2, sm: 3 } }}>
-      <Stack gap={2} alignItems="center">
-        <Grid container spacing={2}>
-          {data.products.map((product) => {
-            return (
-              <Grid key={product.id} item xs={12} sm={6} md={4}>
-                <ProductCard product={product} />
-              </Grid>
-            );
-          })}
-        </Grid>
-        <Pagination
-          count={data.totalPages}
-          page={data.currentPage}
-          renderItem={(item): ReactNode => (
-            <PaginationItem component={Link} to={`/store${item.page === 1 ? '' : `?page=${item.page}`}`} {...item} />
-          )}
-        />
-      </Stack>
-    </Container>
+    <>
+      <Container maxWidth="lg" sx={{ py: { xs: 2, sm: 3 } }}>
+        <Stack gap={2} alignItems="center">
+          <Grid container spacing={2}>
+            {data.products.map((product) => {
+              return (
+                <Grid key={product.id} item xs={12} sm={6} md={4}>
+                  <ProductCard product={product} />
+                </Grid>
+              );
+            })}
+          </Grid>
+          <Pagination
+            count={data.totalPages}
+            page={data.currentPage}
+            renderItem={(item): ReactNode => (
+              <PaginationItem component={Link} to={`/store${item.page === 1 ? '' : `?page=${item.page}`}`} {...item} />
+            )}
+          />
+        </Stack>
+      </Container>
+
+      {isLoading && <Loader />}
+    </>
   );
 };
 
