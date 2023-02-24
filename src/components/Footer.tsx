0f7807ef2c54ca17ddb1fa2a27b4c231a0fa7e72
Samuele Battistella <battistar92@gmail.com>
2023-02-24 13:24:41 +0100

    Fix footer margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Container, Link, Stack, Typography } from '@mui/material';
+import { Box, Container, Link, Stack, Typography } from '@mui/material';
 
 const BUILD_YEAR = 2023;
 const CURRENT_YEAR = new Date().getFullYear();
@@ -12,16 +12,18 @@ const Footer = (): JSX.Element => {
   }
 
   return (
-    <Container component="footer" maxWidth="lg" sx={{ backgroundColor: (theme) => theme.palette.grey[200], p: 1 }}>
-      <Stack direction="row" justifyContent="space-between">
-        <Typography variant="body2" component="span">
-          © {date} Samuele Battistella
-        </Typography>
-        <Link variant="body2" underline="hover" color="inherit" href="https://dummyjson.com/" target="_blank">
-          Powered by dummyjson.com
-        </Link>
-      </Stack>
-    </Container>
+    <Box component="footer" sx={{ backgroundColor: (theme) => theme.palette.grey[200] }}>
+      <Container maxWidth="lg" sx={{ p: 1 }}>
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant="body2" component="span">
+            © {date} Samuele Battistella
+          </Typography>
+          <Link variant="body2" underline="hover" color="inherit" href="https://dummyjson.com/" target="_blank">
+            Powered by dummyjson.com
+          </Link>
+        </Stack>
+      </Container>
+    </Box>
   );
 };
 
