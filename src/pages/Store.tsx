b54a4de1618dff7a92c174c4fac3d6b4cec85bc7
Samuele Battistella <battistar92@gmail.com>
2023-03-06 13:55:44 +0100

    Add SearchBox component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,14 +1,80 @@
-import { Stack, Container, Grid, Pagination, PaginationItem } from '@mui/material';
+import {
+  Stack,
+  Container,
+  Grid,
+  Pagination,
+  PaginationItem,
+  InputBase,
+  IconButton,
+  SxProps,
+  Theme,
+} from '@mui/material';
+import { Search as SearchIcon } from '@mui/icons-material';
 import Loader from 'components/Loader';
 import ProductCard from 'components/ProductCard';
-import { ReactNode, useCallback, useEffect } from 'react';
+import { ChangeEvent, KeyboardEvent, ReactNode, useCallback, useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useProduct } from 'providers/store';
 import { useCart } from 'providers/cart';
 import axios from 'axios';
 import { useSnackbar } from 'notistack';
 
+interface SearchBoxProps {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  onClick: () => void;
+  sx?: SxProps<Theme>;
+}
+
+const SearchBox = ({ value, onChange, onClick, sx }: SearchBoxProps): JSX.Element => {
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+      onChange(event);
+    },
+    [onChange]
+  );
+
+  const handleClick = useCallback((): void => {
+    if (value) {
+      onClick();
+    }
+  }, [value, onClick]);
+
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+      if (value && event.key === 'Enter') {
+        onClick();
+      }
+    },
+    [value, onClick]
+  );
+
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        border: (theme) => `1px solid ${theme.palette.grey[300]}`,
+        borderRadius: 20,
+        px: 2,
+        ...sx,
+      }}
+    >
+      <InputBase
+        placeholder="Search..."
+        value={value}
+        onKeyDown={handleKeyDown}
+        onChange={handleChange}
+        sx={{ flex: 1, color: (theme) => theme.palette.grey[500] }}
+      />
+      <IconButton onClick={handleClick} disabled={value === ''}>
+        <SearchIcon />
+      </IconButton>
+    </Stack>
+  );
+};
+
 const Store = (): JSX.Element => {
+  const [searchText, setSearchText] = useState('');
   const { data, loadPage, isLoading: storeIsLoading } = useProduct();
   const { addToCart, isLoading: cartIsLoading, error: cartError } = useCart();
   const { enqueueSnackbar } = useSnackbar();
@@ -48,10 +114,24 @@ const Store = (): JSX.Element => {
     [addToCart, enqueueSnackbar]
   );
 
+  const onSearchChange = useCallback((event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    setSearchText(event.target.value);
+  }, []);
+
+  const onSearchClick = useCallback((): void => {
+    console.log('search');
+  }, []);
+
   return (
     <>
       <Container maxWidth="lg" sx={{ py: { xs: 2, sm: 3 } }}>
         <Stack gap={2} alignItems="center">
+          <SearchBox
+            value={searchText}
+            onChange={onSearchChange}
+            onClick={onSearchClick}
+            sx={{ width: '100%', maxWidth: (theme) => theme.breakpoints.values.sm }}
+          />
           <Grid container spacing={2}>
             {data.products.map((product) => {
               return (
