6f2e490ab48e27659a24b1194d292b54d3c880a7
Samuele Battistella <battistar92@gmail.com>
2023-03-08 09:42:22 +0100

    Update deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { CssBaseline } from '@mui/material';
 import '@fontsource/roboto/300.css';
@@ -7,10 +7,10 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root') as HTMLElement
 );
